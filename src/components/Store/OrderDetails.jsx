50e4226c9046e46de2245073a0c99ffcb0ad53d3
suresh5189 <suru242000@gmail.com>
2024-06-29 22:32:48 +0530

    File Updated

--- src/components/Store/OrderDetails.jsx
+++ src/components/Store/OrderDetails.jsx
@@ -3,7 +3,7 @@ import { getOrdersDetails } from "../../apiServices";
 import { useParams } from "react-router-dom";
 
 const OrderDetails = () => {
-  const [orderDetails, setOrderDetails] = useState([]);
+  const [orderDetails, setOrderDetails] = useState(null);
 
   const { order_id } = useParams();
 
@@ -12,7 +12,7 @@ const OrderDetails = () => {
       try {
         const refreshToken = localStorage.getItem("RefreshToken");
         const orderDetails = await getOrdersDetails(refreshToken, order_id);
-        console.log("Fetching Order Details", orderDetails.data);
+        // console.log("Fetching Order Details", orderDetails.data.orderData);
         setOrderDetails(orderDetails.data.orderData);
       } catch (error) {
         console.log("Error Fetching Orders Details", error.message);
@@ -23,22 +23,111 @@ const OrderDetails = () => {
 
   return (
     <div>
-      {orderDetails.length > 0 ? (
-        orderDetails.map((detail) => (
-          <div>
-            <h1>Your Order Details</h1>
-            <div>
-              <div>
-                <img src={detail.store_logo} alt="" />
-              </div>
-              <span>{detail.store_name}</span>
+      {orderDetails ? (
+        <div className="OrderDetail">
+          <h1 className="OrderDetailHeading">Your Order Details</h1>
+          <div className="OrderDetailStoreImageAndStore">
+            <div className="OrderDetailStoreImageDiv">
+              <img
+                src={orderDetails.store_logo}
+                alt=""
+                className="OrderDetailStoreImage"
+              />
             </div>
-            <div>
-              <span>orderId</span>
-              <span>Order Status</span>
+            <div className="OrderDetailStoreNameDiv">
+              <span className="OrderDetailStoreName">
+                {orderDetails.store_name}
+              </span>
+              <div className="OrderDetailOrderIdAndStatus">
+                <span className="OrderDetailOrderId">
+                  OrderId: {orderDetails.order_id}
+                </span>
+                <span className="OrderDetailOrderStatus">
+                  OrderStatus: {orderDetails.order_status}
+                </span>
+              </div>
             </div>
           </div>
-        ))
+          <div>
+            {orderDetails.items.map((item) => (
+              <div
+                className="OrderDetailProductImageAndName"
+                key={item.product_id}
+              >
+                <div className="OrderDetailProductImageDiv">
+                  <img
+                    src={item.image}
+                    alt=""
+                    className="OrderDetailProductImage"
+                  />
+                </div>
+                <div className="OrderDetailProductAllDetail">
+                  <div className="OrderDetailProductAllDetailDiv">
+                    <span className="OrderDetailProductName">
+                      Product: {item.title}
+                    </span>
+                    <span className="OrderDetailProductPrice">
+                      Price: {item.price}
+                    </span>
+                    <span className="OrderDetailProductQuantity">
+                      Quantity: {item.quantity}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="OrderDetailAddressAndPayment">
+            <div className="OrderDetailAddressDiv">
+              <span className="OrderDetailAddressType">
+                Type: {orderDetails.address.type}
+              </span>
+              <span className="OrderDetailAddress">
+                {" "}
+                Address: {orderDetails.address.address}
+              </span>
+              <span className="OrderDetailAddressCity">
+                {" "}
+                City: {orderDetails.address.city}
+              </span>
+              <span className="OrderDetailAddressState">
+                {" "}
+                State: {orderDetails.address.state}
+              </span>
+              <span className="OrderDetailAddressCountry">
+                {" "}
+                Country: {orderDetails.address.country}
+              </span>
+              <span className="OrderDetailAddressZipcode">
+                {" "}
+                ZipCode: {orderDetails.address.zip_code}
+              </span>
+              <span className="OrderDetailAddressMobileNumber">
+                Phone No: {orderDetails.mobile_number}
+              </span>
+            </div>
+            <div className="OrderDetailPaymentDiv">
+              <span className="OrderDetailPaymentType">
+                Payment Method: {orderDetails.payment_details.type}
+              </span>
+              <span className="OrderDetailPaymentInvoice">
+                Invoice: {orderDetails.payment_details.invoice}
+              </span>
+              <span className="OrderDetailPaymentBagfee">
+                Bag Fee: ${orderDetails.payment_details.bag_fee}
+              </span>
+              <span className="OrderDetailPaymentDiscount">
+                Discount: {orderDetails.payment_details.discount_applied}%
+              </span>
+              <span className="OrderDetailPaymentServiceFee">
+                Service Fee: ${orderDetails.payment_details.service_fee}
+              </span>
+              <span className="OrderDetailPaymentTotalAmount">
+                Total Amount: ${orderDetails.payment_details.final_subtotal}
+              </span>
+            </div>
+          </div>
+        </div>
       ) : (
         <div>Loading...</div>
       )}
